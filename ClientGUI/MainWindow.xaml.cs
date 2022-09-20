dd30c55d8785a654b013fb35cb7822a518863a61
Kevin Senjaya <kevin.senjaya937@gmail.com>
2022-09-20 14:25:13 +0800

    GUI Updates

--- ClientGUI/MainWindow.xaml.cs
+++ ClientGUI/MainWindow.xaml.cs
@@ -23,6 +23,11 @@ namespace ClientGUI
         private String mode;
         private int token;
         private Dictionary<string, string> methods;
+        private Dictionary<string, int> services;
+        private List<TextBox> textBoxes = new List<TextBox>();
+        private List<TextBlock> textBlocks = new List<TextBlock>();
+        private List<String> namesOfServices = new List<String>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,16 +37,24 @@ namespace ClientGUI
             methods.Add("Login", "authenticator_login_path");
             methods.Add("Register", "authenticator_register_path");
 
-            // get combox box items from registry project - Services (ADDTwoNumbers, ADDThreeNumbers, ect)
-            servicesComboBox.Items.Insert(0, "Please select a service");
-            servicesComboBox.Items.Add("ADDTwoNumbers");
-            servicesComboBox.Items.Add("ADDThreeNumbers");
-            servicesComboBox.Items.Add("MULTwoNumbers");
-            servicesComboBox.Items.Add("MULThreeNumbers");
-            
+            this.services = new Dictionary<string, int>();
+            services.Add("One", 1);
+            services.Add("Two", 2);
+            services.Add("Three", 3);
+            services.Add("Four", 4);
 
+            // get combo box items from registry project - Services (ADDTwoNumbers, ADDThreeNumbers, ect)
+            // add combo box items after authentication
+            servicesComboBox.Items.Add(0);
+            servicesComboBox.Items.Add(1);
+            servicesComboBox.Items.Add(2);
+            servicesComboBox.Items.Add(3);
+            servicesComboBox.Items.Add(4);
         }
 
+
+
+        // Radio Button Functions
         private void registerRadioBtn_Checked(object sender, RoutedEventArgs e)
         {
             mode = "Register";
@@ -69,6 +82,8 @@ namespace ClientGUI
             loginRegisterBtn.Content = mode;
         }
 
+
+        // Login / Register Button Function
         private void loginRegisterBtn_Click(object sender, RoutedEventArgs e)
         {
             String authenticatorPath = methods[mode];
@@ -78,14 +93,28 @@ namespace ClientGUI
             // Save token to private variable
         }
 
+
+        // Combo Box Functions
         private void addComboBoxItems(List<String> services)
         {
+            foreach (String service in services)
+            {
 
+            }
+        }
+
+        private void servicesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int x = (int)servicesComboBox.SelectedItem;
+            createOperandBoxes(x);
+            createHelperTexts(x);
         }
 
         private void createOperandBoxes(int numOfOperands)
         {
+            removeTextBoxes();
             TextBox[] operandTextBoxes = new TextBox[numOfOperands];
+            
             int x = 381;
             int y = 188;
 
@@ -94,10 +123,82 @@ namespace ClientGUI
                 TextBox textBox = new TextBox();
                 operandTextBoxes[i] = textBox;
 
-                textBox.Name = "Operand " + (i + 1).ToString();
+                textBox.Name = "Operand" + (i + 1).ToString();
                 textBox.Text = "Operand " + (i+1).ToString();
-                textBox.Margin = new Thickness(x, y + 27, 0, 0);
-               
+                textBox.Width = 120;
+                textBox.Height = 21.95;
+                textBox.HorizontalAlignment = HorizontalAlignment.Left;
+                textBox.VerticalAlignment = VerticalAlignment.Top;
+
+                textBox.Margin = new Thickness(x, y+=27, 0, 0);
+                operandTextBoxes[i] = textBox;
+            }
+
+            foreach (TextBox textBox in operandTextBoxes)
+            {
+                MainGrid.Children.Add(textBox);
+                textBoxes.Add(textBox);
+            }    
+        }
+
+        private void createHelperTexts(int numOfOperands)
+        {
+            TextBlock[] helperTexts = new TextBlock[numOfOperands];
+            int x = 255;
+            int y = 189;
+
+            for (int i = 0; i < numOfOperands; i++)
+            {
+                TextBlock textBlock = new TextBlock();
+                helperTexts[i] = textBlock;
+
+                textBlock.Name = "HelperText" + (i + 1).ToString();
+                textBlock.Text = "Enter Operand " + (i + 1).ToString() + ": ";
+                textBlock.Width = 110;
+                textBlock.Height = 19.95;
+                textBlock.HorizontalAlignment = HorizontalAlignment.Left;
+                textBlock.VerticalAlignment = VerticalAlignment.Top;
+
+                textBlock.Margin = new Thickness(x, y += 27, 0, 0);
+                helperTexts[i] = textBlock;
+            }
+
+            foreach (TextBlock textBlock in helperTexts)
+            {
+                MainGrid.Children.Add(textBlock);
+                textBlocks.Add(textBlock);
+            }
+        }
+
+        private void removeTextBoxes()
+        {
+            foreach (TextBox textBox in textBoxes)
+            {
+                MainGrid.Children.Remove(textBox);
+            }
+
+            foreach (TextBlock textBlock in textBlocks)
+            {
+                MainGrid.Children.Remove(textBlock);
+            }
+            this.textBoxes.Clear();
+            this.textBlocks.Clear();
+        }
+
+        private void searchServiceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool found = services.ContainsKey(serviceSearchBox.Text);
+            
+            if (found)
+            {
+                bool exists = servicesComboBox.Items.Contains(services[serviceSearchBox.Text]);
+
+                // Search for the matching services from the registry.
+                if (exists)
+                {
+                    servicesComboBox.SelectedItem = services[serviceSearchBox.Text];
+                }
+                
             }
         }
     }
