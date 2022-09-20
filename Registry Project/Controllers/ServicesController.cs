047ddbce75500874fa711c2d2363e651e040e7d5
Scott-jpg <scottpendoley@yahoo.com.au>
2022-09-20 15:35:50 +0800

    Began Implementing Service Register. Moved Service Obj from Service Register to DLL/

---
+++ Registry Project/Controllers/ServicesController.cs
@@ -0,0 +1,44 @@
+﻿using Registry_Project.Models;
+using SOA_SolutionDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Registry_Project.Controllers
+{
+    public class ServicesController : ApiController
+    {
+        // GET: api/Services - Returns all
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        // GET: api/Services/5 - BY ID
+        public IEnumerable<Service> Get(string id)
+        {
+            return new List<Service>();
+        }
+
+        // POST: api/Services
+        [HttpPost]
+        public Result PublishService([FromBody]Service value)
+        {
+            return ServiceToFile.ToFile(value);
+        }
+
+        // PUT: api/Services/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Services/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Registry Project/Models/ServiceToFile.cs
@@ -0,0 +1,57 @@
+﻿using SOA_SolutionDLL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Registry_Project.Models
+{
+    public static class ServiceToFile
+    {
+        private static string root = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName; //Directory containing this solution
+        private static string serviceFile = "Services.txt";
+        private static string servicePath = Path.Combine(root, serviceFile);
+        public static Result ToFile(Service inS)
+        {
+            Result result = new Result();
+
+            using (StreamWriter sw = File.AppendText(servicePath))
+            {
+                sw.WriteLine(
+                    inS.Name + "," +
+                    inS.Description + "," +
+                    inS.APIEndPoint + "," +
+                    inS.NumOfOperands + "," +
+                    inS.OperandType
+                    );
+            }
+            result.Status = Result.ResultCodes.Success;
+            return result;
+        }
+
+        public static IEnumerable<Service> SearchFile(Service inS) 
+        {
+            //Search logic to return services descriptions matching the inS
+            string[] servVals;
+            List<Service> services = new List<Service>();
+
+            string[] lines = File.ReadAllLines(servicePath);
+            foreach (string line in lines)
+            {
+                servVals = line.Split(' ');
+                if (true) 
+                {
+                    services.Add(new Service() { 
+                        Name = servVals[0],
+                        Description = servVals[1],
+                        APIEndPoint = servVals[2],
+                        NumOfOperands = Int16.Parse(servVals[3]),
+                        OperandType = servVals[4]
+                    });
+                }
+            }
+            return new List<Service>();
+        }
+    }
+}
